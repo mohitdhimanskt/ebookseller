63740c01a2794486097b699a213ea1e8db71e602
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-17 16:37:59 +0530

    Add to change route

--- app.js
+++ app.js
@@ -24,4 +24,26 @@ app.get('/', (req, res) => {
   });
 });
 
+// Charge Route
+app.post('/charge', (req, res) => {
+  const amount = 2500;
+  
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'Web Development Ebook',
+    currency: 'usd',
+    customer: customer.id
+  }))
+  .then(charge => res.render('success'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
 
