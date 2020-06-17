c7c8440ad2c5e19a69a60dc10b17cc262c6813a8
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-17 16:32:36 +0530

    Add to handlebars

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+
+
